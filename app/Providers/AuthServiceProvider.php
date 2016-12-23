1cb6b4728bd4e13870fc19519cca96ba4b19404e
Silver Ibenye <silver.ibenye@forrent.com>
2016-12-23 10:39:14 -0500

    Add auth middleware.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,13 +1,12 @@
 <?php
-
 namespace App\Providers;
 
-use App\User;
-use Illuminate\Support\Facades\Gate;
+use App\Models\DAO\User;
 use Illuminate\Support\ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
      *
@@ -29,11 +28,25 @@ class AuthServiceProvider extends ServiceProvider
         // application. The callback which receives the incoming request instance
         // should return either a User instance or null. You're free to obtain
         // the User instance via an API token or any other method necessary.
-
-        $this->app['auth']->viaRequest('api', function ($request) {
-            if ($request->input('api_token')) {
-                return User::where('api_token', $request->input('api_token'))->first();
-            }
-        });
+        $this->app ['auth']->viaRequest('api',
+                function ($request) {
+                    if ($request->header('auth-token') && $request->header('auth-email')) {
+                        return User::where(
+                                [
+                                        [
+                                                'authToken',
+                                                "=",
+                                                $request->header('auth-token')
+                                        ],
+                                        [
+                                                "emailAddress",
+                                                "=",
+                                                $request->header('auth-email')
+                                        ]
+                                ])->first();
+                    } else {
+                        return null;
+                    }
+                });
     }
 }
